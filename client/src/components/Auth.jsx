f2dfa7d2ba4559022765b0626999c9c08a05a00a
Jack Saunders <jack.saunders.home@live.co.uk>
2021-09-28 01:42:59 +0100

    added server functionality

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -4,6 +4,8 @@ import axios from "axios";
 
 import signinImage from "../assets/signup.jpg";
 
+const cookies = new Cookies();
+
 const initialState = {
   fullName: '',
   username: '',
@@ -19,9 +21,33 @@ const Auth = () => {
 
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
-
-    console.log(form);
   };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const { fullName, username, password, phoneNumber, avatarURL } = form;
+
+    const URL = 'http://localhost:5000/auth';
+
+    const { data: { token, userId, hashedPassword } } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
+      username, password, fullName, phoneNumber, avatarURL
+    });
+
+    cookies.set('token', token);
+    cookies.set('username', username);
+    cookies.set('fullName', fullName);
+    cookies.set('userId', userId);
+
+    if(isSignup) {
+      cookies.set('phoneNumber', phoneNumber);
+      cookies.set('avatarURL', avatarURL);
+      cookies.set('hashedPassword', hashedPassword);
+    }
+
+    window.location.reload();
+
+  }
 
   const switchMode = () => {
       setIsSignup((prevIsSignup) => !prevIsSignup);
@@ -32,7 +58,7 @@ const Auth = () => {
       <div className="auth__form-container_fields">
         <div className="auth__form-container_fields-content">
           <p>{isSignup ? "Sign Up" : "Sign In"}</p>
-          <form onSubmit={() => {}}>
+          <form onSubmit={handleSubmit}>
             {isSignup && (
               <div className="auth__form-container_fields-content_input">
                 <label htmlFor="fullName">Full Name</label>
@@ -83,7 +109,7 @@ const Auth = () => {
               <label htmlFor="password">Password</label>
               <input
                 name="password"
-                type="text"
+                type="password"
                 placeholder="Password"
                 onChange={handleChange}
                 required
@@ -94,13 +120,16 @@ const Auth = () => {
                 <label htmlFor="confirmPassword">Confirm Password</label>
                 <input
                   name="confirmPassword"
-                  type="text"
+                  type="password"
                   placeholder="Confirm Password"
                   onChange={handleChange}
                   required
                 />
               </div>
             )}
+            <div className="auth__form-container_fields-content_button">
+              <button>{isSignup ? "Sign Up" : "Sign In"}</button>
+            </div>
           </form>
           <div className="auth__form-container_fields-account">
               <p>
--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -5,7 +5,9 @@ import Cookies from "universal-cookie";
 import ChatIcon from "../assets/chat.png";
 import LogoutIcon from "../assets/logout.png";
 
-const SideBar = () => (
+const cookies = new Cookies();
+
+const SideBar = ({ logout }) => (
     <div className="channel-list__sidebar">
         <div className="channel-list__sidebar__icon1">
             <div className="icon1__inner">
@@ -13,7 +15,7 @@ const SideBar = () => (
             </div>
         </div>
         <div className="channel-list__sidebar__icon2">
-            <div className="icon1__inner">
+            <div className="icon1__inner" onClick={logout}>
                 <img src={LogoutIcon} alt="Logout" width="30" />
             </div>
         </div>
@@ -26,17 +28,34 @@ const CompanyHeader = () => (
     </div>
 );
 
-const ChannelListContainer = () => {
+const ChannelListContainer = ({ isCreating, setIsCreating, setCreateType, setIsEditing }) => {
+    const logout = () => {
+        cookies.remove("token");
+        cookies.remove("userId");
+        cookies.remove("username");
+        cookies.remove("fullName");
+        cookies.remove("avatarURL");
+        cookies.remove("hashedPassword");
+        cookies.remove("phoneNumber");
+
+        window.location.reload();
+    }
     return (
         <>
-            <SideBar />
+            <SideBar logout={logout}/>
             <div className="channel-list__list__wrapper">
                 <CompanyHeader />
                 <ChannelSearch />
                 <ChannelList
                     filters={{}}
                     channelRenderFilterFn={() => { }}
-                    List={(listProps) => (<TeamChannelList {...listProps} type="team" />)}
+                    List={(listProps) => (<TeamChannelList 
+                        {...listProps} 
+                        type="team"
+                        isCreating={isCreating}
+                        setIsCreating={setIsCreating}
+                        setCreateType={setCreateType}
+                        setIsEditing={setIsEditing} />)}
                     Preview={(previewProps) => (
                         <TeamChannelPreview 
                             {...previewProps}
